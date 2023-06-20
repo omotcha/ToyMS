ceff750424182a5cb526cbe98cd8ffcfc7c839ba
omotcha <omotcha2097@gmail.com>
2023-06-20 18:48:39 +0800

    add sample nft

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-waffle");
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.18",
-};
-
-export default config;
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: [
+        `0x632f0b6c7c10c06ae0ecd933a9d87decf0a0c13b7d883400bbf92cfed501215a`
+      ]
+    }
+  },
+  paths: {
+    artifacts: "./src/artifacts",
+  }
+}
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,20 @@
-import { ethers } from "hardhat";
+import hre from 'hardhat'
+import '@nomiclabs/hardhat-ethers'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const [deployer] = await hre.ethers.getSigners();
+  const nfta_contract = await hre.ethers.getContractFactory("NFTA");
+  const nfta_instance = await nfta_contract.deploy();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await nfta_instance.deployed();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("NFTA deployed to:", nfta_instance.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main()
+  .then(()=>process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
 });
